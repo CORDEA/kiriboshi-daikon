e7bbe53582523eb98438afa9ababc2a798c176ec
Yoshihiro Tanaka <contact@cordea.jp>
2021-08-09 14:08:24 +0900

    change interval

--- kiriboshi_daikon/application.py
+++ kiriboshi_daikon/application.py
@@ -1,14 +1,28 @@
 import rumps
 
+TIMER_INTERVAL_TYPE = [
+    15,
+    20,
+    25,
+]
+
 
 class App(rumps.App):
     def __init__(self):
         super(App, self).__init__("Kiriboshi Daikon")
-        self.menu = ["Start timer", "Stop timer"]
-        self.timer = rumps.Timer(self._timer_callback, 3)
+        interval = TIMER_INTERVAL_TYPE[0]
+        self.menu = [rumps.MenuItem("{} minutes".format(interval), self._interval_menu_callback), "Start timer",
+                     "Stop timer"]
+        self.timer = rumps.Timer(self._timer_callback, interval * 60)
 
     def _timer_callback(self, sender):
         print(sender)
+
+    def _interval_menu_callback(self, sender):
+        index = TIMER_INTERVAL_TYPE.index(self.timer.interval / 60)
+        interval = TIMER_INTERVAL_TYPE[0 if index >= len(TIMER_INTERVAL_TYPE) - 1 else index + 1]
+        self.timer.interval = interval * 60
+        sender.title = '{} minutes'.format(interval)
 
     @rumps.clicked("Start timer")
     def start_timer(self, _):
